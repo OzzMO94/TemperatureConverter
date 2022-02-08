3b1c0df6362a6b1532463c10ec10c2094f75663a
OzzMO94 <adinovaaa@yandex.ru>
2022-02-09 01:53:30 +0300

    Initial Commit

---
+++ TemperatureConverter/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TemperatureConverter
+//
+//  Created by Максим Хабиров on 06.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cenliusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+
+    @IBAction func sliderChanged(_ sender: UISlider) {
+    }
+    
+}
+
